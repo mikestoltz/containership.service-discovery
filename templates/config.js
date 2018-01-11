f069a6a1bcd67bff3855eb5b21c348f191295fef
Mike Stoltz <mike.stoltz@containership.io>
2018-01-11 15:02:05 -0500

    nghttpx updates

--- templates/config.js
+++ templates/config.js
@@ -4,24 +4,26 @@ const _ = require('lodash');
 
 module.exports = {
 
-    defaults: {},
+    defaults: { workers: 8 },
 
     render: function(options) {
         _.defaults(options, this.defaults);
 
         const config = _.map(options.applications, (application) => {
             const application_config = [
-                `frontend=*,${application.discovery_port};no-tls`,
+                `frontend=*,${application.env_vars.GRPCLB_PORT};no-tls`,
             ];
 
             _.forEach(application.containers, (container) => {
-                application_config.push(`backend=${container.ip}:${container.port};;no-tls;proto=h2;fall=2;rise=2`);
+                application_config.push(`backend=${container.ip},${container.port};;no-tls;proto=h2;fall=2;rise=2`);
             });
 
-            application_config.push(`workers=${options.workers}`);
+            application_config.push(`workers=${options.workers}\n`);
+
+	    return application_config.join('\n');
         })
 
-        return _.flatten(config).join('/n');
+        return _.flatten(config).join('\n');
     }
 
 };
